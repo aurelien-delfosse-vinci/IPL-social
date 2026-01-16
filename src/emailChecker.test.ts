2a7c445af1533b0f73bca4d6ad3480ebe636b397
aurelien-delfosse-vinci <aurelien.delfosse@student.vinci.be>
2026-01-16 16:40:42 +0100

    refactor: extract emailChecker declaration and extract checks into private functions.

--- src/emailChecker.test.ts
+++ src/emailChecker.test.ts
@@ -2,10 +2,10 @@ import { emit } from "node:cluster";
 import { EmailChecker } from "./emailChecker";
 
 describe('emailChecker', () => {
+    let emailChecker = new EmailChecker();
     describe('isValidEmail method', () => {
 
         it('should return false when the email does not contains at least one @', () => {
-            let emailChecker = new EmailChecker();
 
             let actual = emailChecker.isValidEmail('invalid.com');
 
@@ -13,8 +13,6 @@ describe('emailChecker', () => {
         });
 
         it('should return false when the email has nothing before the @', () => {
-
-            let emailChecker = new EmailChecker();
 
             let actual = emailChecker.isValidEmail('@invalid.com');
 
