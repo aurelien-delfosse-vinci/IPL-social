f24bc456c7a06ecedef78793861b46803b94a1c2
aurelien-delfosse-vinci <aurelien.delfosse@student.vinci.be>
2026-01-16 17:08:58 +0100

    refactor: extracted return check into private dedicated functions

--- src/emailChecker.ts
+++ src/emailChecker.ts
@@ -1,15 +1,31 @@
 export class EmailChecker {
     isValidEmail(email: string): boolean {
         return this.doesEmailContainArobase(email) 
-        && this.doesEmailContainSomethingBeforeArobase(email)
-        && email.indexOf('@') < email.length - 1 
-        && email.includes('.') 
-        && email.indexOf('.') < email.length - 1 
-        && !email.includes(' ');
+        && this.isArobaseFirstCharacter(email)
+        && this.isArobaseLastCharacter(email) 
+        && this.doesEmailContainDot(email)
+        && this.isDotLastCharacter(email) 
+        && !this.doesEmailcontainSpace(email);
 
     }
 
-    private doesEmailContainSomethingBeforeArobase(email: string): boolean {
+    private doesEmailcontainSpace(email: string): boolean {
+        return email.includes(' ');
+    }
+
+    private isDotLastCharacter(email: string) {
+        return email.indexOf('.') < email.length - 1;
+    }
+
+    private doesEmailContainDot(email: string) {
+        return email.includes('.');
+    }
+
+    private isArobaseLastCharacter(email: string) {
+        return email.indexOf('@') < email.length - 1;
+    }
+
+    private isArobaseFirstCharacter(email: string): boolean {
         return email.indexOf('@') > 0;
     }
 
